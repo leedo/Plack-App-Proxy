c14d431c2548fd50eaf673be548a324cab2bf0f0
hiratara <hiratara@cpan.org>
2010-01-11 22:57:19 +0900

    Move the deflate tests.
    Now it'll run on only Server::Standalone.
    I think it doesn't matter.

--- t/Runner.pm
+++ t/Runner.pm
@@ -2,8 +2,6 @@ use strict;
 use warnings;
 use Carp;
 use Plack::App::Proxy;
-use Plack::Middleware::Deflater;
-use IO::Handle::Util qw(:io_from);
 use Plack::Loader;
 use Plack::Test;
 use Test::More;
@@ -57,29 +55,6 @@ sub run_tests {
       my $res = $cb->($req);
       ok $res->is_success, "Check the status line.";
       is $res->content, ('x' x 123) . ('y' x 111), "static proxy";
-    },
-  );
-
-  # Receive the encoded contents.
-  test_proxy(
-    proxy => sub { Plack::App::Proxy->new(host => "http://$_[0]:$_[1]/") },
-    app   => Plack::Middleware::Deflater->wrap(
-      # XXX Plack 0.9030 can't deflate an array-ref response.
-      sub { [ 200, [], io_from_array [ 'Hello ', 'World', "\n" ] ] },
-    ),
-    client => sub {
-      my $cb = shift;
-      # The client send the request to encode the response.
-      my $req = HTTP::Request->new(
-        GET => "http://localhost/index.html", [
-          'Accept-Encoding' => 'gzip,deflate',
-        ]
-      );
-      my $res = $cb->($req);
-      like $res->headers->header('Content-Encoding'), qr/gzip/, 
-           "Recieved Content-Encoding header";
-      is   $res->decoded_content, "Hello World\n",
-           "Recieved the contents gzipped";
     },
   );
 
