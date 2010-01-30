2b12efe528a2c288a663a49ba4981a52179ee9e5
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-01-30 15:16:32 -0800

    Revised document and added middleware config examples. Also added 'plack.proxy.host' environment for the consistency.

--- lib/Plack/App/Proxy.pm
+++ lib/Plack/App/Proxy.pm
@@ -41,7 +41,8 @@ sub build_url_from_env {
     return $self->url->($env)
         if ref $self->url eq 'CODE';
 
-    my $url = ref $self->host eq 'CODE' ? $self->host->($env) : $self->host
+    my $url = $env->{'plack.proxy.host'}
+        || (ref $self->host eq 'CODE' ? $self->host->($env) : $self->host)
         or die "Neither proxy host nor URL are configured";
 
     unless (ref $url eq 'ARRAY') {
@@ -151,28 +152,111 @@ Plack::App::Proxy - proxy requests
 
   use Plack::Builder;
 
+  # proxy all requests for /static to 127.0.0.1:80
   builder {
-      # proxy all requests to 127.0.0.1:80
       mount "/static" => Plack::App::Proxy->new(host => "http://127.0.0.1:80")->to_app;
+  };
 
-      # use some logic to decide which host to proxy to
-      mount "/host" => Plack::App::Proxy->new(host => sub {
-        my $env = shift;
-        ...
-        return $host;
-      })->to_app;
+  # Use middleware to modify requests
+  my $mw = sub {
+      my $app = shift;
+      sub {
+          my $env = shift;
+          ...
+          $env->{'plack.proxy.url'} = $url;
+          $app->($env);
+      };
+  };
 
-      # use some logic to decide what url to proxy
-      mount "/url" => Plack::App::Proxy->new(url => sub {
-        my $env => shift;
-        ...
-        return $url;
-      })->to_app;
+  builder {
+      enable $mw;
+      Plack::App::Proxy->new->to_app;
   };
 
 =head1 DESCRIPTION
 
-Plack::App::Proxy
+Plack::App::Proxy is a middleware-aware proxy application for Plack.
+
+=head1 OPTIONS
+
+=over 4
+
+=item host
+
+  Plack::App::Proxy->new(host => 'http://perl.org/')->to_app;
+
+Specifies the remote proxy host. Don't be tricked by the name 'host',
+since you can also include the URL path as well.
+
+  builder {
+      mount "/example",
+          Plack::App::Proxy->new(host => 'http://example.com/app/foo')->to_app;
+  };
+
+This makes the request for C</example/bar> proxied to
+C<http://example.com/app/foo/bar>.
+
+=head1 preserve_host_header
+
+Preserves the original Host header, which is useful when you do
+reverse proxying to the internal hosts.
+
+=back
+
+=head1 MIDDLEWARE CONFIGURATIONS
+
+This application is just like a normal PSGI application and is
+middleware aware, which means you can modify proxy requests (and
+responses) using Plack middleware stack.
+
+It also supports the following special environment variables:
+
+=over 4
+
+=item plack.proxy.url
+
+Overrides the proxy request URL.
+
+=item plack.proxy.host
+
+Overrides the base URL path to proxy to.
+
+=item plack.proxy.method
+
+Overrides the request method (GET/POST etc.)
+
+=item plack.proxy.headers
+
+Overrides the request headers (as a hash reference)
+
+=item plack.proxy.content
+
+Overrides the request body (as a string).
+
+=back
+
+For example, the following builder code allows you to proxy all GET
+requests to the lolcat image when UNIX epoch time can be multiplied by
+12 (yes, i know it's a silly example) but proxies to the internal host
+otherwise.
+
+  my $mw = sub {
+      my $app = shift;
+      sub {
+          my $env = shift;
+          if ($env->{REQUEST_METHOD} eq 'GET' && time % 12 == 0) {
+              $env->{'plack.proxy.url'} = 'http://lolcat.example.com/lol.png';
+          }
+          $app->($env);
+      };
+  };
+
+  use Plack::Builder;
+
+  builder {
+      enable $mw;
+      Plack::App::Proxy->new(host => 'http://10.0.0.1:8080')->to_app;
+  };
 
 =head1 AUTHOR
  
