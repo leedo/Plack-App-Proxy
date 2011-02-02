3f0a4b31a3a891467ecee10413219f89f0ece039
Robert Buels <rmb32@cornell.edu>
2011-02-01 21:26:12 -0800

    make RewriteLocation middleware support URLMap requests with an optional url_map argument

--- lib/Plack/Middleware/Proxy/RewriteLocation.pm
+++ lib/Plack/Middleware/Proxy/RewriteLocation.pm
@@ -3,6 +3,21 @@ use strict;
 use parent 'Plack::Middleware';
 
 use Plack::Util;
+use Plack::Util::Accessor 'url_map';
+use URI;
+
+sub new {
+    my $self = shift->SUPER::new( @_ );
+
+    # regularize the remote URLs in the URL map
+    if( my $m = $self->url_map ) {
+        for( my $i = 1; $i < @$m; $i += 2 ) {
+            $m->[$i] = $self->_regularize_url( $m->[$i] );
+        }
+    }
+
+    return $self;
+}
 
 sub call {
     my($self, $env) = @_;
@@ -13,16 +28,38 @@ sub call {
         my $cb = $self->app->($env);
         $cb->(sub {
             my $res = shift;
-            if ( my $location = Plack::Util::header_get($res->[1], 'Location') ) {
-                my $remote = ($env->{'plack.proxy.last_url'} =~ m!^(https?://[^/]*/)!)[0];
-                if ($remote && $env->{HTTP_HOST}) {
-                    $location =~ s!^$remote!$env->{'psgi.url_scheme'}://$env->{HTTP_HOST}/!;
+
+            if ( $env->{HTTP_HOST} and my $location = Plack::Util::header_get($res->[1], 'Location') ) {
+
+                # regularize the format of the location so we can
+                # compare it correctly (some apps print this
+                # non-canonically)
+                $location = $self->_regularize_url( $location );
+
+                my @map = (
+                    @{ $self->url_map || [] },
+                    # last-resort url mapping is just the root of the proxy
+                    '/' => ($env->{'plack.proxy.last_url'} =~ m!^(https?://[^/]*/)!)[0],
+                   );
+
+                my $proxy = "$env->{'psgi.url_scheme'}://$env->{HTTP_HOST}";
+
+                while( my ( $proxy_path, $remote ) = splice @map, 0, 2 ) {
+                    last if $location =~ s!^$remote!$proxy$proxy_path!;
                 }
-                Plack::Util::header_set($res->[1], 'Location' => $location);
+
+                $location =~ s!//$!/!; #< avoid double slashes
+
+                Plack::Util::header_set( $res->[1], 'Location' => $location );
             }
-            return $respond->($res);
+
+            return $respond->( $res );
         });
     };
+}
+
+sub _regularize_url {
+    '' . URI->new( $_[1] )->canonical
 }
 
 1;
@@ -43,15 +80,35 @@ Plack::Middleware::Proxy::RewriteLocation - Rewrites redirect headers
       Plack::App::Proxy->new(remote => "http://10.0.1.2:8080/")->to_app;
   };
 
+  ### or, if mounting (i.e. URLMap) the proxied site at /foo
+
+  builder {
+      enable "Proxy::RewriteLocation", url_map => [ '/foo' => http://10.0.1.2:8080' ];
+      mount '/foo' => Plack::App::Proxy->new(remote => "http://10.0.1.2:8080/")->to_app;
+  };
+
 =head1 DESCRIPTION
 
 Plack::Middleware::Proxy::RewriteLocation rewrites the C<Location>
 header in the response when the remote host redirects using its own
 headers, like mod_proxy's C<ProxyPassReverse> option.
 
+=head1 OPTIONS
+
+=over 4
+
+=item url_map (arrayref)
+
+If given, will account for mounted (URLMapped) Proxy apps when
+rewriting C<Location> headers.  Will be applied in order, stopping at the
+first successful match with the remote C<Location>.
+
+=back
+
 =head1 AUTHOR
 
 Tatsuhiko Miyagawa
+Robert Buels
 
 =head1 SEE ALSO
 
--- t/middleware/rewrite.t
+++ t/middleware/rewrite.t
@@ -1,6 +1,7 @@
 use strict;
 use warnings;
 use Plack::App::Proxy;
+use Plack::Builder;
 use Plack::Middleware::Proxy::RewriteLocation;
 use Test::More;
 use Plack::App::Proxy::Test;
@@ -33,6 +34,52 @@ test_proxy(
 
       $res = $cb->(HTTP::Request->new(GET => 'http://localhost/redirect'));
       is $res->header('Location'), 'http://perl.org/';
+  },
+);
+
+
+######
+
+test_proxy(
+    proxy => sub {
+        my ( $host, $port ) = @_;
+        return builder {
+            enable 'Proxy::RewriteLocation', url_map => [
+                '/foo/bar' => "http://$host:$port/uuuugh",
+                '/foo' => "http://$host:$port/noggin",
+               ];
+
+            mount '/foo' => Plack::App::Proxy->new( remote => "http://$host:$port/noggin" );
+            mount '/foo/bar' => sub { [ 402, [], ['oh hai'] ] };
+        };
+    },
+    app   => sub {
+      my $env = shift;
+
+      unless( $env->{PATH_INFO} =~ m!^/noggin! ) {
+          return [ 404, [], 'Not found dude!' ];
+      }
+
+      if( $env->{PATH_INFO} eq '/noggin/redirect' ) {
+          return [ 301, [ Location => 'http://perl.org/' ], [ 'hi' ] ];
+      }
+
+      return [
+          301,
+          [ "Location" => "http://$env->{HTTP_HOST}:80/noggin/redirect", "X-Server-Port" => $env->{SERVER_PORT} ],
+          [ 'Redirected' ],
+         ];
+    },
+  client => sub {
+      my $cb = shift;
+
+      my $res = $cb->( HTTP::Request->new( GET => "http://localhost:80/foo" ) );
+      is $res->code, 301, 'got right status for request at /foo';
+      my $port = $res->header('X-Server-Port');
+      like $res->header('Location'), qr!http://[^/]+/foo/redirect!, 'got right proxied redirect URL';
+
+      $res = $cb->(HTTP::Request->new(GET => 'http://localhost/foo/redirect'));
+      is $res->header('Location'), 'http://perl.org/', 'got right non-proxied redirect URL'
   },
 );
 
