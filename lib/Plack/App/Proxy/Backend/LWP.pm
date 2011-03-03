9ad24ea4c24ad12730dbc7debe01da136bd938b8
Jesse Luehrs <doy@tozt.net>
2011-03-03 11:35:10 -0600

    actually make this streaming

--- lib/Plack/App/Proxy/Backend/LWP.pm
+++ lib/Plack/App/Proxy/Backend/LWP.pm
@@ -8,26 +8,54 @@ sub call {
     my $self = shift;
     my ($env) = @_;
 
-    my $req = HTTP::Request->new(
-        $self->method => $self->url,
-        HTTP::Headers->new(%{ $self->headers }),
-        $self->content
-    );
-    my $ua = LWP::UserAgent->new;
-    my $res = $ua->simple_request($req);
-    # Just assume HTTP::Headers is a blessed hash ref
-    my $headers = +{%{$res->headers}};
-
-    $env->{'plack.proxy.last_protocol'} = '1.1'; # meh
-    $env->{'plack.proxy.last_status'}   = $res->code;
-    $env->{'plack.proxy.last_reason'}   = $res->message;
-    $env->{'plack.proxy.last_url'}      = $self->url;
-
     return sub {
-        my $cb = shift;
-        $cb->([
+        my $respond = shift;
+
+        my $req = HTTP::Request->new(
+            $self->method => $self->url,
+            HTTP::Headers->new(%{ $self->headers }),
+            $self->content
+        );
+
+        my $ua = LWP::UserAgent->new;
+        my $writer;
+
+        $ua->add_handler(
+            response_header => sub {
+                my ($res) = @_;
+                # Just assume HTTP::Headers is a blessed hash ref
+                my $headers = { %{ $res->headers } };
+
+                $env->{'plack.proxy.last_protocol'} = '1.1'; # meh
+                $env->{'plack.proxy.last_status'}   = $res->code;
+                $env->{'plack.proxy.last_reason'}   = $res->message;
+                $env->{'plack.proxy.last_url'}      = $self->url;
+
+                $writer = $respond->([
+                    $res->code,
+                    [$self->response_headers->($headers)],
+                ]);
+            },
+        );
+        $ua->add_handler(
+            response_data => sub {
+                my (undef, undef, undef, $data) = @_;
+                $writer->write($data);
+                return 1;
+            },
+        );
+        $ua->add_handler(
+            response_done => sub {
+                $writer->close if $writer;
+            },
+        );
+
+        my $res = $ua->simple_request($req);
+        return if $writer;
+        $respond->([
             $res->code,
-            [$self->response_headers->($headers)],
+            # Just assume HTTP::Headers is a blessed hash ref
+            [$self->response_headers->({ %{ $res->headers } })],
             [$res->content],
         ]);
     };
