1793ccb7fc85e7610659fb710d7bd9378bc16c2c
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2014-01-24 22:22:25 +0100

    The optional "ua" parameter allows to use customized User Agent object

--- lib/Plack/App/Proxy/Test.pm
+++ lib/Plack/App/Proxy/Test.pm
@@ -29,6 +29,7 @@ sub test_proxy {
     my $app    = delete $args{app}    or croak "app needed";
     my $proxy  = delete $args{proxy}  or croak "proxy needed";
     my $host   = delete $args{host} || '127.0.0.1';
+    my $ua     = delete $args{ua}   || Plack::LWPish->new( max_redirect => 0 );
 
     for my $backend (@BACKENDS) {
 
@@ -41,8 +42,7 @@ sub test_proxy {
                     app => $proxy->( $host, $port ),
                     client => $client,
                     host => $host,
-                    # disable the auto redirection of LWP::UA
-                    ua => Plack::LWPish->new( max_redirect => 0 ),
+                    ua => $ua,
                 );
             },
             server => sub {
@@ -91,6 +91,7 @@ test_psgi of Plack::Test simply.
 
   test_proxy app    => $app, 
              proxy  => $proxy_cb->($app_host, $app_port), 
+             ua     => LWP::UserAgent->new, 
              client => $client_cb->($cb);
 
 =back
@@ -100,6 +101,9 @@ In 'C<proxy>' callback, you should create the proxy server instance to send
 requests to 'C<app>' server. Then 'C<client>' callback is called to run your 
 tests. In 'C<client>' callback, all HTTP requests are sent to 'C<proxy>' 
 server. (And the proxy server will proxy your request to the app server.)
+
+The optional 'C<ua>' parameter allows to use customized User Agent
+object. L<Plack::LWPish> object is used by default.
 
 =head1 AUTHOR
 
